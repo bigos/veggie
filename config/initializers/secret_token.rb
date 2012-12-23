dcd167b754f832a6b2679ff9e6c50dca782b574b
Jacek <ruby.object@googlemail.com>
2012-12-23 01:38:40 +0000

    created new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Veggie::Application.config.secret_token = '7837d99cffe919e9514ca3076be1e3d61dedde008791bb3171b2c1a6de16ac456da8c0efdce4c264f8fc89d830ec3bb8fd23d7ff119a14bf365a3c17733ae6f8'
