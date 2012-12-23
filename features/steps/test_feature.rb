a0577e74070c9889e391d14296e4cf7ba1f95742
Jacek Podkanski <ruby.object@googlemail.com>
2012-12-23 18:05:13 +0000

    after running RAILS_ENV=test rake db:migrate first step has passed

--- features/steps/test_feature.rb
+++ features/steps/test_feature.rb
@@ -1,9 +1,9 @@
 class Spinach::Features::TestFeature < Spinach::FeatureSteps
   step 'I visit the posts page' do
-    visit posts_path
+    visit '/posts'
   end
 
   step 'I should see "Posts"' do
-    page.has_content?('Posts').must_equal true
+    page.has_content?('posts').must_equal true
   end
 end
