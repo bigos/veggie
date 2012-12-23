cb5e1f56a0e7dcba1f549f62dcb17a6476298ff5
Jacek Podkanski <ruby.object@googlemail.com>
2012-12-23 19:07:11 +0000

    first success using Capybara with Spinach

--- features/steps/test_feature.rb
+++ features/steps/test_feature.rb
@@ -1,9 +1,12 @@
+require 'spinach/capybara'
 class Spinach::Features::TestFeature < Spinach::FeatureSteps
-  step 'I visit the posts page' do
-    visit '/posts'
+  step 'I visit the posts page' do    
+    visit '/posts'    
+    p current_path
   end
-
   step 'I should see "Posts"' do
-    page.has_content?('posts').must_equal true
+    #display html content of tested page
+    p page.html
+    page.has_content? 'posts'
   end
 end
