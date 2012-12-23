67cfbb46ebaf74742fd9187fd98ff20645c0c311
Jacek Podkanski <ruby.object@googlemail.com>
2012-12-23 21:36:30 +0000

    modified test to get correct clean output

--- features/steps/test_feature.rb
+++ features/steps/test_feature.rb
@@ -2,14 +2,14 @@ require 'spinach/capybara'
 class Spinach::Features::TestFeature < Spinach::FeatureSteps
   Given 'I visit the posts page' do    
     visit '/posts'
-    p current_path
+    #p current_path
   end
   Then 'I should see "Posts"' do
-    p page.methods.sort
+    #p page.methods.sort
     #display html content of tested page
-    p page.body
+    #p page.body
     5.should eq(5) #rspeck expectations
     5.should_not eq(4)
-    page.body.should include('pzzzzzzzzzzzzzzosts')
+    page.body.should include('posts')
   end
 end
