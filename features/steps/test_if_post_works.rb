3c5299b981a25e6f4c2771c2c265713427957087
Jacek <ruby.object@googlemail.com>
2012-12-23 03:24:14 +0000

    automatically generated steps from the feature

---
+++ features/steps/test_if_post_works.rb
@@ -0,0 +1,17 @@
+class Spinach::Features::TestIfPostWorks < Spinach::FeatureSteps
+  step 'I have an empty form' do
+    pending 'step not implemented'
+  end
+
+  step 'I fill it in with content' do
+    pending 'step not implemented'
+  end
+
+  step 'I click Submit' do
+    pending 'step not implemented'
+  end
+
+  step 'the post content should be displayed in the post list' do
+    pending 'step not implemented'
+  end
+end
