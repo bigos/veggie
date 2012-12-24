c6c745374e7a623513d495ffca5fa589b5da4f6e
Jacek Podkanski <ruby.object@googlemail.com>
2012-12-24 23:44:04 +0000

    generated the steps

---
+++ features/steps/commenting_to_a_post.rb
@@ -0,0 +1,41 @@
+class Spinach::Features::CommentingToAPost < Spinach::FeatureSteps
+  step 'I visit the posts page' do
+    pending 'step not implemented'
+  end
+
+  step 'select a post' do
+    pending 'step not implemented'
+  end
+
+  step 'I should view the post' do
+    pending 'step not implemented'
+  end
+
+  step 'I click comment link' do
+    pending 'step not implemented'
+  end
+
+  step 'I should go to new_comment_path' do
+    pending 'step not implemented'
+  end
+
+  step 'fill the text area with Test Comment.' do
+    pending 'step not implemented'
+  end
+
+  step 'I click link Create Comment' do
+    pending 'step not implemented'
+  end
+
+  step 'I should see flash saying comment was successfully created.' do
+    pending 'step not implemented'
+  end
+
+  step 'I should be redirected to comment view page' do
+    pending 'step not implemented'
+  end
+
+  step 'page content should contain Test Comment' do
+    pending 'step not implemented'
+  end
+end
